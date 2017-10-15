7b6cf5ecc14b2781689074fa255067b66a744ab8
Ashank Anshuman <ashankanshuman@gmail.com>
2017-10-15 16:53:24 +0530

    basic structure

--- Dc_downloader.py
+++ Dc_downloader.py
@@ -6,42 +6,67 @@
 5. songs (album) (torrent) - 4
 6. softwares - (free, paid) - 5
 """
+import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
 class Dc_downloader:
 
 	def __init__ (self, dlink, dtype, dname) :
-		self.download_link = dlink
-		self.dowload_type = dtype
-		self.download_name = dname
+		self.dlink = dlink
+		self.dtype = dtype
+		self.dname = dname
 	
 
 	def process_download(self):
 		# Check if dlink exists
-		if not self.dlink:
-			direct_download(self.dlink)
+		if self.dlink:
+			self.direct_download()
 			return
 		# Process dtype here
 		if self.dtype == 0:
 			# Process youtube downloads here
+			pass
 		elif self.dtype == 1:
 			# Process torrent downloads here
+			pass
 		elif self.dtype == 2:
 			# Process direct downloads here
+			pass
 		elif self.dtype == 3:
 			# Process anime downloads here
+			pass
 		elif self.dtype == 4:
 			# Process songs downloads here
+			pass
 		elif self.dtype == 5:
-			# Process software downloads here
+			# Process software downloads here (free)
+			pass
+			self.free_software_download()
+		elif self.dtype == 6:
+			# Process software downloads here (paid)
+			pass
 		else:
+			pass
 			# Mark request for manual processing
 
-	def direct_download(self, dlink) :
+	def direct_download(self):
 		# Process download here
+		pass
 
+	def free_software_download(self):
+		url = "http://filehippo.com/search?q="
+		file_name = re.sub('\s+', '+', self.dname)
+		url += file_name
+		print (url)
+		page = urlopen(url)
 
 
 def main():
 	# TODO implement command line parser and get input from user
+	dlink = ""
+	dtype = 5
+	dname = "Google chrome"
 	download_link = dlink
 	dowload_type = dtype
 	download_name = dname
